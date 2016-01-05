0c89310266cf8b98b30368ca729b96a3056bb301
Rai Kashif <13004065101@umt.edu.pk>
2016-01-05 20:24:04 +0500

    BST junit test code file

---
+++ Test/BSTTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class BSTTest {
+	
+     BST bst = null;
+     
+     @Before
+     public void initilize(){
+    	 bst  =  new BST();
+     }
+
+}
